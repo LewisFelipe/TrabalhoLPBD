5243af096f6c9fb6489f8b3fa0837f58571486ba
mulatoana <anazoccamulato@gmail.com>
2024-05-15 17:37:53 -0300

    criacao php

--- DataBase.sql
+++ DataBase.sql
@@ -7,3 +7,27 @@ CREATE TABLE `lpbd`.`users`(
     PRIMARY KEY (`id`),
     UNIQUE `UQ_USERNAME` (`username`)
 ) ENGINE = InnoDB;
+
+CREATE TABLE `lpbd`.`pontos`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `idUser` INT NOT NULL,
+    `dataHora` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`idUser`) REFERENCES users(id)
+) ENGINE = InnoDB;
+
+CREATE TABLE `lpbd`.`atestados`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `idUser` INT NOT NULL,
+    `nome` VARCHAR(180) NOT NULL,
+    `cpf` VARCHAR(16) NOT NULL,
+    `cid` VARCHAR(8) NOT NULL,
+    `dataAtestado` VARCHAR(10) NOT NULL,
+    `qtdDias` VARCHAR(3) NOT NULL,
+    `obs` VARCHAR(255) NOT NULL,
+    `arquivoBase64` VARCHAR(8000) NULL,
+    `dataEnvioAtestado` VARCHAR(20) NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`idUser`) REFERENCES users(id)
+) ENGINE = InnoDB;
+
